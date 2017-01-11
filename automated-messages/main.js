c1a99a6a2fa1356a7621cf48e2e98d697eeda0f5
Daranix <Daranix@users.noreply.github.com>
2017-01-11 01:09:27 +0100

    Another way :)
    Is more beatiful

--- automated-messages/main.js
+++ automated-messages/main.js
@@ -8,28 +8,41 @@ console.log('[Automated-Messages] Online!');
 const chat = jcmp.events.Call('get_chat')[0];
 
 /*
-To add a new broadcast Just copy paste these lines
-
-msg[0] = function(){
-	chat.broadcast('1');
-}
-
-Make sure to change [0] to the next number!
+	To add a new broadcast just add a new line into the array like this:
+	
+	COLOR 
+	
+	var msg = [
+		{ message: 'Your new message', color: RGB(0,255,0) }, // YOUR NEW LINE
+		{ message: 'Your new message' }, // Color is optional by default is white
+		{ message: 'Welcome to the server!', color: RGB(0,255,0) },
+		{ message: 'Rules: Have fun!' , color: new RGB(0,0,255) }
+	];
+	
 */
 
-var msg = [];
+var msg = [
+	{ message: 'Welcome to the server!', color: RGB(0,255,0) },
+	{ message: 'Rules: Have fun!' , color: new RGB(0,0,255) }
+];
 
-msg[0] = function(){
-	chat.broadcast('Welcome to the server!');
-}
-msg[1] = function(){
+/*
+	Or you can do here
+	msg.push({ message: 'Your message', color: new RGB(255,0,0) });
+*/
 
-	chat.broadcast('Rules: Have fun!');
-}
+
 
 //By default it'll pick a random one every 5 minutes!
-var minutes = 5, the_interval = minutes * 60 * 1000;
+var minutes = 5
+var the_interval = minutes * 60 * 1000;
 setInterval(function() {
 	var rand = parseInt(Math.random()*msg.length);
-	msg[rand]();
+	
+	if(typeof(msg[rand].color === 'undefined') {
+	   msg[rand].color = new RGB(255,255,255);
+	}
+	    
+	chat.broadcast(msg[rand].message, msg[rand].color);
+
 }, the_interval);
